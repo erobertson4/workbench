9feac47ad3ffe06c2f2d53c19e0a424942667dda
erobertson4 <elijahrobertson@ameritech.net>
2014-06-14 22:03:38 -0400

    Adding line 4

--- src/workbench/HelloWorld.java
+++ src/workbench/HelloWorld.java
@@ -4,7 +4,8 @@ public class HelloWorld {
 
   public static void main(String[] args) {
     System.out.println("Hello");
-    System.out.println("Time for some fun!");    
+    System.out.println("Time for some fun!");
+    System.out.println("Line 4");
   }
 
 }
